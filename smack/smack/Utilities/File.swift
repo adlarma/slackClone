444a8c4b61291e135e536bf98b0a3690e44eb3ba
Adler <adler@ese.plus>
2020-10-28 16:08:02 -0600

    with login screen

---
+++ smack/smack/Utilities/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  smack
+//
+//  Created by Adler Martinez on 10/28/20.
+//
+
+import Foundation
+
+let toLogin = "toLogin"
---
+++ smack/smack/View/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Adler Martinez on 10/28/20.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
