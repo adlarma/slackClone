54efedf03f1b05c110de86eb45234acd549795bd
Adler <adler@ese.plus>
2020-10-28 12:07:02 -0600

    Added slide menu

---
+++ smack/smack/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Adler Martinez on 10/28/20.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+
+
+
+}
